07e90387c3c05ea26644d1e475683a19e74f57a8
Sâm <samntt@ahamove.com>
2021-07-13 12:34:32 +0700

    Update

--- Topics/Loop control statements/Process integer input/main.py
+++ Topics/Loop control statements/Process integer input/main.py
@@ -1,1 +1,12 @@
 # put your python code here
+num_list = []
+while True:
+    num = int(input())
+    if num < 10:
+        continue
+    elif num > 100:
+        break
+    else:
+        num_list.append(num)
+for num in num_list:
+    print(num)
